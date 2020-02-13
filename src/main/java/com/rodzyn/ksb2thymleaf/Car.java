937689dc292869b861646ad3f3d383bb70ac7c93
overment <mateusz.swiniarski@interia.pl>
2020-02-13 21:34:03 +0100

    add new element

--- src/main/java/com/rodzyn/ksb2thymleaf/Car.java
+++ src/main/java/com/rodzyn/ksb2thymleaf/Car.java
@@ -28,4 +28,12 @@ public class Car {
     public void setModel(String model) {
         this.model = model;
     }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "mark='" + mark + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/rodzyn/ksb2thymleaf/CarController.java
+++ src/main/java/com/rodzyn/ksb2thymleaf/CarController.java
@@ -3,6 +3,8 @@ package com.rodzyn.ksb2thymleaf;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,16 +13,33 @@ import java.util.List;
 @Controller
 public class CarController {
 
-    @GetMapping("/car")
-    public String getCar(Model model){
+    private List<Car> cars = new ArrayList<>();
+
+    public CarController() {
         Car car1 = new Car("BMW", "i8");
         Car car2 = new Car("Fiat", "126p");
         Car car3 = new Car("Polonez", "Caro");
         Car car4 = new Car("Nissan", "Sunny");
 
-        List<Car> cars = Arrays.asList(car1, car2, car3, car4);
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
+    }
+
+    @GetMapping("/car")
+    public String getCar(Model model){
+
         model.addAttribute("name", "Mateusz");
         model.addAttribute("cars", cars);
+        model.addAttribute("newCar", new Car());
         return "car";
+    }
+
+    @PostMapping("add-car")
+    public String addCar(@ModelAttribute Car car){
+        cars.add(car);
+        System.out.println(car);
+        return "redirect:/car";
     }
 }
